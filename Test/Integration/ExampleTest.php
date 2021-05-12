442031340ba2aab80248420c53adc07415793a70
Michiel Gerritsen <michiel@controlaltdelete.nl>
2021-05-12 15:24:32 +0200

    Tests setup

---
+++ Test/Integration/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CM\Payments\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true, 'This test should always succeed');
+    }
+}
---
+++ Test/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CM\Payments\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true, 'This test should always succeed');
+    }
+}
