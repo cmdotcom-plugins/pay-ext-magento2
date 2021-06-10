c1ab0f6e86768cba8d29828acc76855a14477641
Stijn van der Vegt <stijnvandervegt@gmail.com>
2021-06-10 14:03:47 +0200

    fix Magento 2.3 intgration tests

--- Test/Integration/Service/OrderServiceTest.php
+++ Test/Integration/Service/OrderServiceTest.php
@@ -4,24 +4,18 @@ namespace CM\Payments\Test\Integration\Service;
 
 use CM\Payments\Api\Client\ApiClientInterface;
 use CM\Payments\Api\Model\Data\OrderInterfaceFactory;
-use CM\Payments\Api\Model\Domain\CMOrderInterfaceFactory;
 use CM\Payments\Api\Model\OrderRepositoryInterface as CMOrderRepositoryInterface;
 use CM\Payments\Api\Service\OrderServiceInterface;
 use CM\Payments\Service\OrderRequestBuilder;
 use CM\Payments\Service\OrderService;
+use CM\Payments\Test\Integration\IntegrationTestCase;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\OrderRepository;
-use Magento\TestFramework\ObjectManager;
 use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\TestCase;
 
-class OrderServiceTest extends TestCase
+class OrderServiceTest extends IntegrationTestCase
 {
-    /**
-     * @var ObjectManager
-     */
-    private $objectManager;
     /**
      * @var ApiClientInterface|MockObject
      */
@@ -34,16 +28,8 @@ class OrderServiceTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->objectManager = ObjectManager::getInstance();
 
         $this->clientMock = $this->createMock(ApiClientInterface::class);
-
-        $this->clientMock->expects($this->once())->method('execute')->willReturn([
-            'order_key' => '0287A1617D93780EF28044B98438BF2F',
-            //phpcs:ignore
-            'url' => 'https://testsecure.docdatapayments.com/ps/menu?merchant_name=itonomy_b_v&client_language=NL&payment_cluster_key=0287A1617D93780EF28044B98438BF2F',
-            'expires_on' => '2021-07-12T08:10:57Z'
-        ]);
 
         $this->orderService = $this->objectManager->create(OrderService::class, [
             'orderRepository' => $this->objectManager->create(OrderRepository::class),
@@ -55,33 +41,44 @@ class OrderServiceTest extends TestCase
         ]);
     }
 
-    /**
-     * @magentoDataFixture Magento/Sales/_files/order_complete.php
-     */
     public function testCreateOrder()
     {
+        $this->clientMock->expects($this->once())->method('execute')->willReturn([
+            'order_key' => '0287A1617D93780EF28044B98438BF2F',
+            //phpcs:ignore
+            'url' => 'https://testsecure.docdatapayments.com/ps/menu?merchant_name=itonomy_b_v&client_language=NL&payment_cluster_key=0287A1617D93780EF28044B98438BF2F',
+            'expires_on' => '2021-07-12T08:10:57Z'
+        ]);
+
+        $this->loadFixture('Magento/Sales/order_complete.php');
         $magentoOrder = $this->loadOrderById('100000333');
 
         $order = $this->orderService->create($magentoOrder->getId());
         $this->assertSame(
-            //phpcs:ignore
+        //phpcs:ignore
             'https://testsecure.docdatapayments.com/ps/menu?merchant_name=itonomy_b_v&client_language=NL&payment_cluster_key=0287A1617D93780EF28044B98438BF2F',
             $order->getUrl()
         );
     }
 
-    /**
-     * @magentoDataFixture Magento/Sales/_files/order_complete.php
-     */
     public function testSaveOrderReferenceInDatabase()
     {
+        $this->clientMock->expects($this->once())->method('execute')->willReturn([
+            'order_key' => '2287A1617D93780EF28044B98438BF2F',
+            //phpcs:ignore
+            'url' => 'https://testsecure.docdatapayments.com/ps/menu?merchant_name=itonomy_b_v&client_language=NL&payment_cluster_key=0287A1617D93780EF28044B98438BF2F',
+            'expires_on' => '2021-07-12T08:10:57Z'
+        ]);
+
+        $this->loadFixture('Magento/Sales/order_complete.php');
+
         $magentoOrder = $this->loadOrderById('100000333');
         $this->orderService->create($magentoOrder->getId());
 
         /** @var OrderRepositoryInterface $cmOrderRepository */
         $cmOrderRepository = $this->objectManager->create(CMOrderRepositoryInterface::class);
 
-        $result = $cmOrderRepository->getByOrderKey('0287A1617D93780EF28044B98438BF2F');
+        $result = $cmOrderRepository->getByOrderKey('2287A1617D93780EF28044B98438BF2F');
 
         $this->assertSame($magentoOrder->getId(), $result->getOrderId());
     }
