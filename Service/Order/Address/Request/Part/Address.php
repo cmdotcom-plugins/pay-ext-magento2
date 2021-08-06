42a050fc390c8cd5ee8c9264b29e45e9052e7ce9
Ivan Tagil <ivan.tagil@itonomy.nl>
2021-08-06 11:18:04 +0300

    CMP-86 - Added address service

--- Service/Order/Address/Request/Part/Address.php
+++ Service/Order/Address/Request/Part/Address.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace CM\Payments\Service\Order\Address\Request\Part;
 
+use CM\Payments\Api\Service\AddressServiceInterface;
 use CM\Payments\Api\Service\Shopper\Request\RequestPartByOrderAddressInterface;
 use CM\Payments\Client\Model\Request\ShopperCreate;
 use Magento\Sales\Api\Data\OrderAddressInterface;
@@ -15,21 +16,38 @@ use Magento\Sales\Api\Data\OrderAddressInterface;
 class Address implements RequestPartByOrderAddressInterface
 {
     /**
+     * @var AddressServiceInterface
+     */
+    private $addressService;
+
+    /**
+     * Address constructor
+     *
+     * @param AddressServiceInterface $addressService
+     */
+    public function __construct(
+        AddressServiceInterface $addressService
+    ) {
+        $this->addressService = $addressService;
+    }
+
+    /**
      * @inheritDoc
      */
     public function process(OrderAddressInterface $orderAddress, ShopperCreate $shopperCreate): ShopperCreate
     {
-        //TODO: Replace on separate interface with properties
-        //TODO: Replace on proper data (based on separate service by recognizing this data from Street
-        $shopperCreate->setAddress([
-                                       'street' => implode('', $orderAddress->getStreet()),
-                                       'housenumber' => '5',
-                                       'housenumber_addition' => 'w',
-                                       'postalcode' => $orderAddress->getPostcode(),
-                                       'city' => $orderAddress->getCity(),
-                                       'state' => $orderAddress->getRegionCode(),
-                                       'country' => $orderAddress->getCountryId(),
-                                   ]);
+        $addressObject = [
+            'street' => implode('', $orderAddress->getStreet()),
+            'housenumber' => '',
+            'housenumber_addition' => '',
+            'postalcode' => $orderAddress->getPostcode(),
+            'city' => $orderAddress->getCity(),
+            'state' => $orderAddress->getRegionCode(),
+            'country' => $orderAddress->getCountryId()
+        ];
+
+        $addressObject = $this->addressService->process($addressObject);
+        $shopperCreate->setAddress(array_filter($addressObject));
 
         return $shopperCreate;
     }
--- Service/Quote/Address/Request/Part/Address.php
+++ Service/Quote/Address/Request/Part/Address.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace CM\Payments\Service\Quote\Address\Request\Part;
 
+use CM\Payments\Api\Service\AddressServiceInterface;
 use CM\Payments\Api\Service\Shopper\Request\RequestPartByQuoteAddressInterface;
 use CM\Payments\Client\Model\Request\ShopperCreate;
 use Magento\Quote\Api\Data\AddressInterface;
@@ -15,21 +16,38 @@ use Magento\Quote\Api\Data\AddressInterface;
 class Address implements RequestPartByQuoteAddressInterface
 {
     /**
+     * @var AddressServiceInterface
+     */
+    private $addressService;
+
+    /**
+     * Address constructor
+     *
+     * @param AddressServiceInterface $addressService
+     */
+    public function __construct(
+        AddressServiceInterface $addressService
+    ) {
+        $this->addressService = $addressService;
+    }
+
+    /**
      * @inheritDoc
      */
     public function process(AddressInterface $quoteAddress, ShopperCreate $shopperCreate): ShopperCreate
     {
-        //TODO: Replace on separate interface with properties
-        //TODO: Replace on proper data (based on separate service by recognizing this data from Street
-        $shopperCreate->setAddress([
-                                       'street' => implode('', $quoteAddress->getStreet()),
-                                       'housenumber' => '5',
-                                       'housenumber_addition' => 'w',
-                                       'postalcode' => $quoteAddress->getPostcode(),
-                                       'city' => $quoteAddress->getCity(),
-                                       'state' => $quoteAddress->getRegionCode(),
-                                       'country' => $quoteAddress->getCountryId(),
-                                   ]);
+        $addressObject = [
+            'street' => implode('', $quoteAddress->getStreet()),
+            'housenumber' => '',
+            'housenumber_addition' => '',
+            'postalcode' => $quoteAddress->getPostcode(),
+            'city' => $quoteAddress->getCity(),
+            'state' => $quoteAddress->getRegionCode(),
+            'country' => $quoteAddress->getCountryId()
+        ];
+
+        $addressObject = $this->addressService->process($addressObject);
+        $shopperCreate->setAddress(array_filter($addressObject));
 
         return $shopperCreate;
     }
