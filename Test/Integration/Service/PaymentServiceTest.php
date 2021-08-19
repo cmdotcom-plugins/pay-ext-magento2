c783e740f836c86bcf3ebc507312f4c8cc8da915
Stijn van der Vegt <stijnvandervegt@gmail.com>
2021-08-19 22:18:12 +0200

    add payment service test for card payments

--- Test/Integration/Service/PaymentServiceTest.php
+++ Test/Integration/Service/PaymentServiceTest.php
@@ -16,10 +16,24 @@ use CM\Payments\Api\Service\PaymentServiceInterface;
 use CM\Payments\Client\Api\ApiClientInterface;
 use CM\Payments\Client\Model\CMPaymentFactory;
 use CM\Payments\Client\Model\CMPaymentUrlFactory;
+use CM\Payments\Client\Model\Response\ShopperCreate;
+use CM\Payments\Client\Order;
 use CM\Payments\Client\Payment;
 use CM\Payments\Logger\CMPaymentsLogger;
+use CM\Payments\Model\Data\BrowserDetails;
+use CM\Payments\Model\Data\CardDetails;
+use CM\Payments\Service\OrderRequestBuilder;
+use CM\Payments\Service\OrderService;
 use CM\Payments\Service\PaymentRequestBuilder;
 use CM\Payments\Service\PaymentService;
+use CM\Payments\Service\Quote\Request\Part\Amount;
+use CM\Payments\Service\Quote\Request\Part\BillingAddressKey;
+use CM\Payments\Service\Quote\Request\Part\Country;
+use CM\Payments\Service\Quote\Request\Part\Currency;
+use CM\Payments\Service\Quote\Request\Part\Email;
+use CM\Payments\Service\Quote\Request\Part\Language;
+use CM\Payments\Service\Quote\Request\Part\OrderId;
+use CM\Payments\Service\ShopperService;
 use CM\Payments\Test\Integration\IntegrationTestCase;
 use Magento\Sales\Model\OrderRepository;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -35,6 +49,11 @@ class PaymentServiceTest extends IntegrationTestCase
      * @var PaymentServiceInterface
      */
     private $paymentService;
+
+    /**
+     * @var ShopperService|MockObject
+     */
+    private $shopperServiceMock;
 
     /**
      * @magentoDataFixture Magento/Sales/_files/order.php
@@ -142,6 +161,71 @@ class PaymentServiceTest extends IntegrationTestCase
     }
 
     /**
+     * @magentoDataFixture Magento/Sales/_files/quote.php
+     */
+    public function testCardPayment()
+    {
+        $magentoQuote = $this->loadQuoteById('test01');
+        $magentoQuote->setIsActive(true);
+        $magentoQuote->save();
+        $magentoQuote->reserveOrderId();
+
+        $this->shopperServiceMock->expects($this->once())->method('createByQuoteAddress')->willReturn(
+            new ShopperCreate(['shopper_key' => '123', 'address_key' => '123'])
+        );
+
+        $this->clientMock->expects($this->exactly(2))->method('execute')->willReturnOnConsecutiveCalls(
+            [
+                'order_key' => '0287A1617D93780EF28044B98438BF2M',
+                //phpcs:ignore
+                'url' => 'https://testsecure.docdatapayments.com/ps/menu?merchant_name=itonomy_b_v&client_language=NL&payment_cluster_key=0287A1617D93780EF28044B98438BF2F',
+                'expires_on' => '2021-07-12T08:10:57Z'
+            ],
+            [
+                'id' => 'pid4911261022t',
+                'status' => 'REDIRECTED_FOR_AUTHENTICATION',
+                'redirect_url' => null,
+                'urls' => [
+                    [
+                        //phpcs:ignore
+                        'url' => 'https =>//testsecure.docdatapayments.com/ps/api/public/3dsv2/v1/transactions/3ds-method-notification',
+                        'order' => '1',
+                        'method' => 'POST',
+                        'purpose' => 'HIDDEN_IFRAME'
+                    ],
+                    [
+                        //phpcs:ignore
+                        'url' => 'https =>//testsecure.docdatapayments.com/ps/api/public/3dsv2/v1/transactions/2637baac-fe7c-46b2-b895-d21aef765342/references/4911288290/authenticate',
+                        'order' => '2',
+                        'method' => 'POST',
+                        'purpose' => 'IFRAME'
+                    ]
+                ]
+            ]
+        );
+
+        $cardDetails = new CardDetails();
+        $cardDetails->setEncryptedCardData('encrypted_dummy_data');
+        $cardDetails->setMethod('MC');
+
+        $browserDetails = new BrowserDetails();
+        $browserDetails
+            ->setShopperIp('0.0.0.0')
+            ->setAccept('text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+            //phpcs:ignore
+            ->setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363');
+
+        $payment = $this->paymentService->createByCardDetails(
+            (int) $magentoQuote->getId(),
+            $cardDetails,
+            $browserDetails
+        );
+        $this->assertNotNull(
+            $payment->getId()
+        );
+    }
+
+    /**
      * @magentoDataFixture Magento/Sales/_files/order.php
      */
     public function testSavePaymentInDatabase()
@@ -196,12 +280,46 @@ class PaymentServiceTest extends IntegrationTestCase
                 'apiClient' => $this->clientMock,
             ]
         );
+        $orderClient = $this->objectManager->create(
+            Order::class,
+            [
+                'apiClient' => $this->clientMock,
+            ]
+        );
+
+        $this->shopperServiceMock = $this->createMock(ShopperService::class);
+        $billingAddressKey = $this->objectManager->create(BillingAddressKey::class, [
+            'shopperService' => $this->shopperServiceMock
+        ]);
+
+        $orderRequestBuilder = $this->objectManager->create(
+            OrderRequestBuilder::class,
+            [
+                'quoteRequestParts' => [
+                    $billingAddressKey,
+                    $this->objectManager->create(OrderId::class),
+                    $this->objectManager->create(Amount::class),
+                    $this->objectManager->create(Country::class),
+                    $this->objectManager->create(Currency::class),
+                    $this->objectManager->create(Email::class),
+                    $this->objectManager->create(Language::class)
+                ]
+            ]
+        );
+        $orderService = $this->objectManager->create(
+            OrderService::class,
+            [
+                'orderClient' => $orderClient,
+                'orderRequestBuilder' => $orderRequestBuilder
+            ],
+        );
 
         $this->paymentService = $this->objectManager->create(
             PaymentService::class,
             [
                 'orderRepository' => $this->objectManager->create(OrderRepository::class),
                 'paymentClient' => $paymentClient,
+                'orderService' => $orderService,
                 'paymentRequestBuilder' => $this->objectManager->create(PaymentRequestBuilder::class),
                 'cmPaymentDataFactory' => $this->objectManager->create(CMPaymentDataFactory::class),
                 'cmPaymentFactory' => $this->objectManager->create(CMPaymentFactory::class),
