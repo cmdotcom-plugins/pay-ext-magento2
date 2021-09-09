d72c538b057678a9f44ce01c5f1209d6253ca067
Ivan Tagil <ivan.tagil@itonomy.nl>
2021-09-09 08:50:28 +0300

    CMP-83 - Bug fixing

---
+++ Block/Adminhtml/System/Config/Fieldset/PaymentMethods.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © CM.com. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace CM\Payments\Block\Adminhtml\System\Config\Fieldset;
+
+use Magento\Config\Block\System\Config\Form\Fieldset;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class PaymentMethods extends Fieldset
+{
+    /**
+     * Return header html for fieldset
+     *
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getHeaderHtml($element)
+    {
+        return parent::_getHeaderHtml($element) .
+            '<div class="payment-methods-extra" ' .
+            'data-mage-init=\'{"CM_Payments/js/system/action/payment-methods-config":' .
+            '{"mainContainerSelector": "#config-edit-form"}}\'></div>';
+    }
+}
