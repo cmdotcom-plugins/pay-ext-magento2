04228e67257cda4164b1d7bc40d9ac8db2d30ef4
Stijn van der Vegt <stijnvandervegt@gmail.com>
2021-09-24 12:21:23 +0200

    fix namespace

--- Webapi/PaymentMethodManagement.php
+++ Webapi/PaymentMethodManagement.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace CM\Payments\Model;
+namespace CM\Payments\Webapi;
 
 use CM\Payments\Api\PaymentMethodManagementInterface;
 use Magento\Checkout\Api\Data\PaymentDetailsInterface;
