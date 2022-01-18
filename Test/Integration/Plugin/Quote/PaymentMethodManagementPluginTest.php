3fe2da37e7ea8d9a8dad11ce1539e4ca152198d5
Stijn van der Vegt <stijnvandervegt@gmail.com>
2022-01-18 09:34:47 +0100

    fix test

--- Test/Integration/Plugin/Quote/PaymentMethodManagementPluginTest.php
+++ Test/Integration/Plugin/Quote/PaymentMethodManagementPluginTest.php
@@ -13,30 +13,16 @@ use CM\Payments\Model\ConfigProvider;
 use CM\Payments\Plugin\Quote\PaymentMethodManagementPlugin;
 use CM\Payments\Service\MethodService;
 use CM\Payments\Service\Order\Request\Part\Amount;
-use CM\Payments\Service\Order\Request\Part\BillingAddressKey;
-use CM\Payments\Service\Order\Request\Part\Country;
-use CM\Payments\Service\Order\Request\Part\Currency;
-use CM\Payments\Service\Order\Request\Part\Email;
-use CM\Payments\Service\Order\Request\Part\Expiry;
 use CM\Payments\Service\Order\Request\Part\Language;
 use CM\Payments\Service\Order\Request\Part\OrderId;
-use CM\Payments\Service\Order\Request\Part\PaymentProfile;
-use CM\Payments\Service\Order\Request\Part\ReturnUrls;
 use CM\Payments\Service\OrderRequestBuilder;
 use CM\Payments\Service\Quote\Request\Part\Amount as QuoteAmount;
-use CM\Payments\Service\Quote\Request\Part\BillingAddressKey as QuoteBillingAddressKey;
-use CM\Payments\Service\Quote\Request\Part\Country as QuoteCountry;
-use CM\Payments\Service\Quote\Request\Part\Currency as QuoteCurrency;
-use CM\Payments\Service\Quote\Request\Part\Email as QuoteEmail;
-use CM\Payments\Service\Quote\Request\Part\Expiry as QuoteExpiry;
-use CM\Payments\Service\Quote\Request\Part\Language as QuoteLanguage;
 use CM\Payments\Service\Quote\Request\Part\OrderId as QuoteOrderId;
-use CM\Payments\Service\Quote\Request\Part\PaymentProfile as QuotePaymentProfile;
-use CM\Payments\Service\Quote\Request\Part\ReturnUrls as QuoteReturnUrls;
 use CM\Payments\Test\Integration\IntegrationTestCase;
+use Magento\Quote\Api\Data\PaymentMethodInterface;
 use Magento\Quote\Api\PaymentMethodManagementInterface;
 
-class AddMethodsAdditionalDataPaymentTest extends IntegrationTestCase
+class PaymentMethodManagementPluginTest extends IntegrationTestCase
 {
     /**
      * @var MethodService
@@ -66,11 +52,7 @@ class AddMethodsAdditionalDataPaymentTest extends IntegrationTestCase
             ['methodService' => $this->methodService]
         );
 
-        $this->clientMock->expects($this->exactly(1))->method('execute')->willReturnOnConsecutiveCalls(
-            [
-                'shopper_key' => 'ec11cd24-e667-4f9e-a677-5ffe0d4aee5e',
-                'address_key' => 'ec11cd24-e667-4f9e-a677-5ffe0d4aee5e'
-            ],
+        $this->clientMock->expects($this->exactly(3))->method('execute')->willReturnOnConsecutiveCalls(
             [
                 'order_key' => '2287A1617D93780EF28044B98438BF2F',
                 //phpcs:ignore
@@ -78,14 +60,21 @@ class AddMethodsAdditionalDataPaymentTest extends IntegrationTestCase
                 'expires_on' => '2021-07-12T08:10:57Z'
             ],
             [],
-            $this->getMethodResponse()
+            [
+                [
+                    'method' => 'IDEAL',
+                ],
+                [
+                    'method' => 'KLARNA',
+                ]
+            ]
         );
 
         $subject = $this->objectManager->create(PaymentMethodManagementInterface::class);
         $magentoQuote = $this->loadQuoteById('test01');
         $magentoMethods = [
+            $this->getPaymentMethod(ConfigProvider::CODE),
             $this->getPaymentMethod(ConfigProvider::CODE_IDEAL),
-            $this->getPaymentMethod(ConfigProvider::CODE_KLARNA),
             $this->getPaymentMethod('checkmo')
         ];
 
@@ -107,88 +96,46 @@ class AddMethodsAdditionalDataPaymentTest extends IntegrationTestCase
         parent::setUp();
 
         $this->clientMock = $this->createMock(ApiClientInterface::class);
-        $orderClient = $this->objectManager->create(
-            Order::class,
-            [
-                'apiClient' => $this->clientMock,
-            ]
-        );
-
+        $orderClient = $this->objectManager->create(Order::class, ['apiClient' => $this->clientMock]);
         $orderRequestBuilder = $this->objectManager->create(OrderRequestBuilder::class, [
             'orderRequestParts' => [
                 $this->objectManager->create(OrderId::class),
                 $this->objectManager->create(Amount::class),
-                $this->objectManager->create(Currency::class),
+                $this->objectManager->create(\CM\Payments\Service\Order\Item\Request\Part\Currency::class),
                 $this->objectManager->create(Language::class),
-                $this->objectManager->create(Country::class),
-                $this->objectManager->create(PaymentProfile::class),
-                $this->objectManager->create(Email::class),
-                $this->objectManager->create(ReturnUrls::class),
-                $this->objectManager->create(Expiry::class),
-                $this->objectManager->create(
-                    BillingAddressKey::class,
-                    [
-                        'shopperService' => $this->shopperService
-                    ]
-                ),
             ],
             'quoteRequestParts' => [
                 $this->objectManager->create(QuoteOrderId::class),
                 $this->objectManager->create(QuoteAmount::class),
-                $this->objectManager->create(QuoteCurrency::class),
-                $this->objectManager->create(QuoteLanguage::class),
-                $this->objectManager->create(QuoteCountry::class),
-                $this->objectManager->create(QuotePaymentProfile::class),
-                $this->objectManager->create(QuoteEmail::class),
-                $this->objectManager->create(QuoteReturnUrls::class),
-                $this->objectManager->create(QuoteExpiry::class),
-                $this->objectManager->create(
-                    QuoteBillingAddressKey::class,
-                    [
-                        'shopperService' => $this->shopperService
-                    ]
-                ),
             ]
         ]);
 
-        $this->orderService = $this->objectManager->create(OrderServiceInterface::class, [
-            'orderRequestBuilder' =>  $orderRequestBuilder,
+        $orderService = $this->objectManager->create(OrderServiceInterface::class, [
+            'orderRequestBuilder' => $orderRequestBuilder,
             'orderClient' => $orderClient
         ]);
 
         $this->methodService = $this->objectManager->create(
             MethodService::class,
             [
-                'orderClient' => $orderClient,
-                'orderService' => $this->orderService
+                'orderService' => $orderService,
+                'orderClient' => $orderClient
             ]
         );
     }
 
     /**
-     * @return array[]
+     * @param string $code
      */
-    private function getMethodResponse()
+    private function getPaymentMethod(string $code): PaymentMethodInterface
     {
-        return [
-            [
-                'method' => 'IDEAL',
-                'ideal_details' => [
-                    'issuers' => [
-                        [
-                            'id' => 'BUNQNL2A',
-                            'name' => 'bunq'
-                        ],
-                        [
-                            'id' => 'ASNBNL21',
-                            'name' => 'ASN Bank'
-                        ]
-                    ],
-                ],
-            ],
-            [
-                'method' => 'BANCONTACT',
-            ]
-        ];
+        $paymentMethodMock = $this->getMockBuilder(PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $paymentMethodMock->method('getCode')->willReturn($code);
+        $paymentMethodMock->method('getTitle')->willReturn($code);
+
+        return $paymentMethodMock;
     }
 }
